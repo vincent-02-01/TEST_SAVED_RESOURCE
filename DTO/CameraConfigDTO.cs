82669060cca9bcc5f4540be02e6069132e3ddb2f
lh-ha <lh-ha@asti.com.vn>
2025-09-13 11:16:47 +0700

    first commit

---
+++ DTO/CameraConfigDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ASTI_Vision.DTO
+{
+    public class CameraConfigDTO
+    {
+        public string SerialNumber { get; set; }
+        public string ModelName { get; set; }
+        public long Width { get; set; }
+        public long Height { get; set; }
+        public double ExposureTime { get; set; }
+        public string ExposureAuto { get; set; }
+        public double Gain { get; set; }
+        public string GainAuto { get; set; }
+        public double Gamma { get; set; }
+        public string PixelFormat { get; set; }
+        public string WhiteBalanceAuto { get; set; }
+        public bool AcqusitionFrameRateEnable { get; set; }
+    }
+}
---
+++ DTO/LightingDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace ASTI_Vision.DTO
+{
+    public class ChannelDTO
+    {
+        public int Channel { get; set; }
+        public int Intensity { get; set; }
+        public bool IsOn { get; set; }
+    }
+
+    public class ExportDTO
+    {
+        public Parity Parity { get; set; }
+        public StopBits StopBits { get; set; }
+        public int BaudRate { get; set; }
+        public int DataBits { get; set; }
+
+        public List<ChannelDTO> Channels { get; set; } = new List<ChannelDTO>();
+    }
+}
